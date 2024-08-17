846372e5069c407482d25acd733c1ee1359cd048
decidev22 <hamento22@gmail.com>
2024-08-17 15:04:42 +1000

    test commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx,html}",
+    "./components/**/*.{js,jsx,ts,tsx,html}",
+    "./scripts/**/*.{js,jsx,ts,tsx,html}",
+    "./constants/**/*.{js,jsx,ts,tsx,html}",
+    "./hooks/**/*.{js,jsx,ts,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
